e892e2c9ef9e892295181244e9830bbeaa4d7a0e
Shunichiro Aki <register.develop@nana4.net>
2021-09-11 15:22:56 +0900

    モードにBlast(送風)を追加

--- dst/lib/model/EoliaOperationMode.d.ts
+++ dst/lib/model/EoliaOperationMode.d.ts
@@ -1,6 +1,8 @@
 /** モード
 * 停止 | 自動 | 冷房 | 暖房 | 除湿
-* 冷房除湿 | 衣類乾燥 | 送風 | おそうじ | おでかけクリーン
+* 冷房除湿 | 衣類乾燥 | 送風 | ナノイー | おそうじ | おでかけクリーン
+*
+* 送風かつnanoex=trueの場合、ナノイー扱いになる？
 */
-export declare type EoliaOperationMode = 'Stop' | 'Auto' | 'Cooling' | 'Heating' | 'ComfortableDehumidification' | 'CoolDehumidifying' | 'ClothesDryer' | 'Nanoe' | 'Cleaning' | 'NanoexCleaning';
+export declare type EoliaOperationMode = 'Stop' | 'Auto' | 'Cooling' | 'Heating' | 'ComfortableDehumidification' | 'CoolDehumidifying' | 'ClothesDryer' | 'Blast' | 'Nanoe' | 'Cleaning' | 'NanoexCleaning';
 //# sourceMappingURL=EoliaOperationMode.d.ts.map
